45038fc458def9b19c4341ca573d6f63efe00490
Gustavo Santos <gustavo.santos.lima@hotmail.com>
2024-05-13 10:20:24 -0300

    Fix: change brand section

--- src/app/components/SectionScreen.jsx
+++ src/app/components/SectionScreen.jsx
@@ -1,8 +1,12 @@
 export default function SectionScreen({ id, text }) {
   return (
     <section id={`section-${id}`} className="section-screen">
-      <div className="flex-1"></div>
-      <div className="flex-1 pl-8">
+      <div className="flex-1">
+        <h1 className="text-right text-4xl sm:text-6xl md:text-7xl uppercase">
+          Brand =
+        </h1>
+      </div>
+      <div className="flex-1 pl-4">
         <span className="text-4xl sm:text-6xl md:text-7xl">{text}</span>
       </div>
     </section>
--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,14 @@
-import SectionBrand from "./components/SectionBrand";
 import SectionScreen from "./components/SectionScreen";
 
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center justify-center">
-      <div className="flex items-center justify-center relative">
-        <SectionBrand brand="Brand" />
-
-        <div className="snap-scroll scroll-smooth">
-          <SectionScreen id={1} text="Section 1" />
-          <SectionScreen id={2} text="Section 2" />
-          <SectionScreen id={3} text="Section 3" />
-          <SectionScreen id={4} text="Section 4" />
-          <SectionScreen id={5} text="Section 5" />
-        </div>
+      <div className="snap-scroll scroll-smooth">
+        <SectionScreen id={1} text="Section 1" />
+        <SectionScreen id={2} text="Section 2" />
+        <SectionScreen id={3} text="Section 3" />
+        <SectionScreen id={4} text="Section 4" />
+        <SectionScreen id={5} text="Section 5" />
       </div>
     </main>
   );
